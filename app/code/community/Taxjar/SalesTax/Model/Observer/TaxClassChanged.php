fec0ec53cd80cdb714b746a0034634c674e9d3f6
Trey Trimble <bagaintrey@gmail.com>
2018-08-02 16:10:57 -0400

    Only run observer when it is customer tax class not product

--- app/code/community/Taxjar/SalesTax/Model/Observer/TaxClassChanged.php
+++ app/code/community/Taxjar/SalesTax/Model/Observer/TaxClassChanged.php
@@ -19,39 +19,42 @@ class Taxjar_SalesTax_Model_Observer_TaxClassChanged
 {
     public function preDispatch(Varien_Event_Observer $observer)
     {
-        $isExempt = Mage::app()->getRequest()->getParam('tj_salestax_code');
-        if ('99999' != $isExempt) {
-            Mage::app()->getRequest()->setPost('tj_salestax_exempt_type', Taxjar_SalesTax_Helper_Data::DEFAULT_EXEMPTION_TYPE);
+        if ('CUSTOMER' == Mage::app()->getRequest()->getParam('class_type')) {
+            $isExempt = Mage::app()->getRequest()->getParam('tj_salestax_code');
+            if ('99999' != $isExempt) {
+                Mage::app()->getRequest()->setPost('tj_salestax_exempt_type', Taxjar_SalesTax_Helper_Data::DEFAULT_EXEMPTION_TYPE);
+            }
+            Mage::register('original_tax_class', Mage::getModel('tax/class')->load(Mage::app()->getRequest()->getParam('class_id'))->getData());
         }
-        Mage::register('original_tax_class', Mage::getModel('tax/class')->load(Mage::app()->getRequest()->getParam('class_id'))->getData());
     }
 
     public function postDispatch(Varien_Event_Observer $observer)
     {
-        $classId = Mage::app()->getRequest()->getParam('class_id');
-        $taxClass = Mage::getModel('tax/class')->load($classId);
-        $originalData = Mage::registry('original_tax_class');
-        Mage::unregister('original_tax_class'); // Remove from register just in case 
-        
-        // On change we need to loop through all the affected customers and sync/resync them
-        // First verify that it's a change we care about
-        $customers = Mage::getModel('customer/customer')->getCollection()->addFieldToFilter('group_id', $taxClass->getId())->addAttributeToSelect('*');
+        if ('CUSTOMER' == Mage::app()->getRequest()->getParam('class_type')) {
+            $classId = Mage::app()->getRequest()->getParam('class_id');
+            $taxClass = Mage::getModel('tax/class')->load($classId);
+            $originalData = Mage::registry('original_tax_class');
+            Mage::unregister('original_tax_class'); // Remove from register just in case
 
-        if ('' == $taxClass->getTjSalestaxCode() && '' == $originalData['tj_salestax_code']) {
-            // * No -> No -- Don't sync
-            return;
-        }  else if (('99999' == $taxClass->getTjSalestaxCode() && '99999' == $originalData['tj_salestax_code']) && ($taxClass->getTjSalestaxExemptType() == $originalData['tj_salestax_exempt_type'])) {
-            // * Yes -> Yes -- Sync as exemption type
-            return;
+            // On change we need to loop through all the affected customers and sync/resync them
+            // First verify that it's a change we care about
+            $customers = Mage::getModel('customer/customer')->getCollection()->addFieldToFilter('group_id', $taxClass->getId())->addAttributeToSelect('*');
+
+            if ('' == $taxClass->getTjSalestaxCode() && '' == $originalData['tj_salestax_code']) {
+                // * No -> No -- Don't sync
+                return;
+            } else if (('99999' == $taxClass->getTjSalestaxCode() && '99999' == $originalData['tj_salestax_code']) && ($taxClass->getTjSalestaxExemptType() == $originalData['tj_salestax_exempt_type'])) {
+                // * Yes -> Yes -- Sync as exemption type
+                return;
+            }
+
+            // This process can take awhile
+            @set_time_limit(0);
+            @ignore_user_abort(true);
+            foreach ($customers as $customer) {
+                Mage::getModel('taxjar/client_customerSync', $customer)->syncUpdates();
+            }
         }
-
-        // This process can take awhile
-        @set_time_limit(0);
-        @ignore_user_abort(true);
-        foreach ($customers as $customer) {
-            Mage::getModel('taxjar/client_customerSync', $customer)->syncUpdates();
-        }
-
     }
 
 }
