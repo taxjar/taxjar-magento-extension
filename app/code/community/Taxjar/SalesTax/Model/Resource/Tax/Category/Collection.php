5dd00f6d01ab97a1fa2b60ca35cbbb7006b5a73f
Ryan Reeves <ryan.reeves@taxjar.com>
2020-01-15 12:19:41 -0700

    Rework product tax category storage
    Remove use of array_column to support < PHP 5.0.0

--- app/code/community/Taxjar/SalesTax/Model/Resource/Tax/Category/Collection.php
+++ app/code/community/Taxjar/SalesTax/Model/Resource/Tax/Category/Collection.php
@@ -32,6 +32,7 @@ class Taxjar_SalesTax_Model_Resource_Tax_Category_Collection extends Mage_Core_M
      */
     public function toOptionArray()
     {
+        $labels = array();
         $options = array(
             array(
                 'label' => 'Fully Taxable',
@@ -47,7 +48,10 @@ class Taxjar_SalesTax_Model_Resource_Tax_Category_Collection extends Mage_Core_M
             );
         }
 
-        $labels = array_column($options, 'label');
+        foreach($options as $option) {
+            $labels[] = $option['label'];
+        }
+
         array_multisort($labels, SORT_ASC, $options);
 
         return $options;
